2916ef953eae7fef96a8703efa493fc769611ba1
Aseel-Dweedar <archaseel.1992@gmail.com>
2021-04-13 22:27:20 +0300

    edit index.html & app.js & style.css, and add readme file

---
+++ class-02/README.md
@@ -0,0 +1,20 @@
+# ABOUT ME
+## Class-02 (12/4/2021)
+
+* Creat all files and folders needed for this project  
+    * File: (inedx.html , style.css app.js)
+    * Folders (img , js , css)
+    * add helpful files (eslintrc.json, .gitignore, .markdownlint)
+* statr working in the web page : layout, main design , home page and add requirement (paragraph about me).
+* style page with css.
+* Add javascript requirements (5 yes or no questions),
+and other operations on it.
+<br>
+<hr>
+
+## Class-03 (13/4/2021)
+
+* working in HTML page : add ordered list with my top ten and un ordered list with education and experiences.
+* style page with css.
+* Add javascript requirements (6th and 7th questions yes or no questions), and other operations on it.
+* Edit read me file with very day working.
--- class-02/js/app.js
+++ class-02/js/app.js
@@ -1,9 +1,8 @@
 'use strict';
-
+let score = 0;
 let userName = prompt('Hello, What is your name?');
 
 alert('Hello ' + userName + ', Lets play Yes or No game ^_^');
-
 
 let answer1 = prompt('Do you think I have a twin?').toLowerCase();
 while (answer1 !== 'yes' && answer1 !== 'y' && answer1 !== 'no' && answer1 !== 'n') {
@@ -14,6 +13,12 @@ if (answer1 == 'yes' || answer1 == 'y') {
 } else {
     alert('correct, I do not');
 }
+
+if (answer1 == 'no' || answer1 == 'n') {
+    score = score + 1;
+
+}
+
 // console.log('Do you think I have a twin? Your answer is ' + answer1);
 
 
@@ -26,6 +31,11 @@ if (answer2 == 'yes' || answer2 == 'y') {
 } else {
     alert('Sorry, But I do :D');
 }
+if (answer2 == 'yes' || answer2 == 'y') {
+    score = score + 1;
+
+}
+
 // console.log('Do I love movies? Your answer is ' + answer2);
 
 
@@ -38,6 +48,11 @@ if (answer3 == 'yes' || answer3 == 'y') {
 } else {
     alert('Unfortunately, you are right :( ');
 }
+if (answer3 == 'no' || answer3 == 'n') {
+    score = score + 1;
+
+}
+
 // console.log('Do you think i am Healthy and sporty? Your answer is ' + answer3);
 
 
@@ -50,7 +65,12 @@ if (answer4 == 'yes' || answer4 == 'y') {
 } else {
     alert('I did, Just once');
 }
-// console.log('Do you think I travel before? Your answer is ' + answer4);
+if (answer4 == 'yes' || answer4 == 'y') {
+    score = score + 1;
+
+}
+
+//console.log('Do you think I travel before? Your answer is ' + answer4);
 
 
 let answer5 = prompt('Do you think I believe in ghosts?').toLowerCase();
@@ -62,6 +82,71 @@ if (answer5 == 'yes' || answer5 == 'y') {
 } else {
     alert('Sure i dont');
 }
+if (answer5 == 'no' || answer5 == 'n') {
+    score = score + 1;
+
+}
+
 // console.log('Do you think I believe in ghosts? Your answer is ' + answer5);
 
-alert('Thank you ' + userName + ', more about me in the website ^_^');
+alert('Thank you ' + userName + ', Here is one more game ^_^');
+
+let guessNumber = prompt('Can you guess the number of my Nephews?');
+let c;
+for (c = 0; c < 3; c++) {
+    if (guessNumber == 3) {
+        alert('That is correct ^-^');
+        c = c + 3;
+    } else if (guessNumber >= 6) {
+        guessNumber = prompt('too high, try again..');
+    } else if (guessNumber <= 1) {
+        guessNumber = prompt('too low, try again..');
+    } else if (guessNumber < 6 && guessNumber > 1) {
+        guessNumber = prompt('So close, try again..');
+    } else {
+        guessNumber = prompt('Please enter a number');
+    }
+}
+
+if (c == 3) {
+    if (guessNumber == 3) {
+        alert('That is correct ^-^');
+    } else {
+        alert('The right answer is 3, Good luck next time.');
+    }
+}
+
+if (guessNumber == 3) {
+    score = score + 1;
+}
+
+// console.log('Can you guess the number of my Nephews? Your answer is ' + guessNumber);
+
+let guessName = prompt('I have 3 favorite months, Can you guess one of them?').toLowerCase();
+let d;
+let months = ['april', 'september', 'november'];
+
+for (d = 0; d < 5; d++) {
+    if (guessName == 'april' || guessName == 'september' || guessName == 'november') {
+        alert('That is correct ^-^, My favorite months are, April, September & November');
+        d = d + 5;
+    } else {
+        guessName = prompt('Not really, try again').toLowerCase();
+    }
+}
+
+if (d == 5) {
+    if (guessName == 'april' || guessName == 'september' || guessName == 'november') {
+        alert('That is correct ^-^, My favorite months are, April, September & November');
+    } else {
+        alert('Unfortunately, But My favorite months are, April, September & November');
+    }
+}
+
+if (guessName == 'april' || guessName == 'september' || guessName == 'november') {
+    score = score + 1;
+}
+
+// console.log('Can you guess the number of my Nephews? Your answer is ' + guessName);
+
+alert('your score is : ' + score + '/7');
